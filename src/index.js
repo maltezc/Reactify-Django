2ac10047b03460328701b3f857bcb8bda346e03a
cflux1990 <cflux.maltez@live.com>
2018-07-03 06:12:12 -0700

    Just added functionality for 2 different apps to run separately temporarily

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+let myComponent = document.getElementById('reactify-django-ui')
+if (myComponent !== null) {
+    ReactDOM.render(<App />, myComponent);
+}
+
+let myComponent2 = document.getElementById('reactify-django-ui-2')
+if (myComponent2 !==null) {
+    ReactDOM.render(<App/>, myComponent2)
+}
+
+// how react renders out app that we have created...which is a componentrendering out another component
+// registerServiceWorker();
